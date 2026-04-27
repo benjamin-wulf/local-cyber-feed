55507b31723fcb750063650d3e5bcc7f84b20021
unknown <contact@benjaminwulf.com>
2026-04-27 16:15:06 -0400

    improved guid as url logic

--- logic/reader.py
+++ logic/reader.py
@@ -1,6 +1,8 @@
 import feedparser
 import time
 import hashlib
+import bleach
+import re
 from urllib.parse import urlparse, urlunparse
 
 
@@ -28,39 +30,79 @@ def SanitizeURL(dirtyURL):
 
     return cleanURL
 
+def SanitizeContent(rawContent):
+    # strip tags: <script>, <style>, <iframe>, <embed>, <object>, <form>, <meta>
+    # strip attributes: onclick, onload, onerror, style
+
+    # can keep: <p>, <br>, <ul>, <ol>, <li>, <blockquote>
+    # formatting: <b>, <strong>, <i>, <em>, <u>
+    # links: <a> (but force target="_blank" AND rel="noopener noreferrer")
+
+    # decide how to handle <img> later
+
+    allowedTags = ['p', 'b', 'i', 'strong', 'em', 'a', 'ul', 'ol', 'li', 'br', 'blockquote']
+    allowedAttributes = {
+        'a': ['href', 'title', 'rel'],
+        'img': ['src', 'alt', 'title']
+    }
+
+    # might make sentences at the end of paragraphs clash without a space. "BeautifulSoup" package is an option, or replace </p> maybe?
+    cleanContent = bleach.clean(rawContent, tags=allowedTags, attributes=allowedAttributes, strip=True, strip_comments=True)
+
+    #print("CLEAN CONTENT: " + cleanContent)
+    return cleanContent
+
+# if the feed is only supplying a brief summary, this may not always detect updates
+def GetContentHash(rawContent):
+
+    ultraCleanContent = bleach.clean(rawContent, tags=[], strip=True, strip_comments=True)
+    ultraCleanContent = ultraCleanContent.lower()
+
+    # regex is probably overkill for this but who cares
+    ultraCleanContent = re.sub(r"\s+", "", ultraCleanContent)
+
+    contentHash = hashlib.sha256(ultraCleanContent.encode('utf-8')).hexdigest()
+    return contentHash
+
 def SanitizeFeed(feed):
 
     for entry in feed.entries:   
 
-        print(feed.feed.get('title')) # RSS Feed Title , testing is Bleeping Computer
-        print(entry.get('title')) # Article Titles
+        print("RSS Feed Title: " + feed.feed.get('title')) # RSS Feed Title , testing is Bleeping Computer
+        print("Article Title: " + entry.get('title')) # Article Titles
 
         cleanArticleLink = SanitizeURL(entry.get('link'))
-        print(cleanArticleLink)
+        print("Clean Article Link: " + cleanArticleLink)
         
         if entry.get('published_parsed'):
             cleanDate = time.strftime('%Y-%m-%d %H:%M:%S', entry.published_parsed)
         else:
             cleanDate = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         
-        print(cleanDate)
+        print("Date: " + cleanDate)
 
         # get summary
+        rawContent = ""
         if entry.get('content'):
-            print(entry.get('content'))
+            rawContent = entry.get('content')
         elif entry.get('summary'):
-            print(entry.get('summary'))
+            rawContent = entry.get('summary')
         else:
-            print("No summary available")
+            rawContent = "No summary available."
+
+        print("Cleaned Content is: " + SanitizeContent(rawContent))
+        print("Content Hash is: " + GetContentHash(rawContent))
             
         # I don't like when the ID is just the url, so taking the hash of the url instead
-        guid = entry.get('id')
-        if "https://" in guid:
+        if entry.get('guidislink'):
             # feed is using link as guid, trust publisher
-            print(hashlib.sha256(guid.encode('utf-8')).hexdigest())
+            print("Hashed ID is: " + hashlib.sha256(entry.guid.encode('utf-8')).hexdigest())
+        elif all([urlparse(entry.guid).scheme in ['http', 'https'], urlparse(entry.guid).netloc]):
+            # feed doesn't have guidislink set, but guid does appear to be a link
+            print("Hashed ID is: " + hashlib.sha256(entry.guid.encode('utf-8')).hexdigest())
         else:
-            # feed is not, use our normalized/sanitized link
-            print(hashlib.sha256(cleanArticleLink.encode('utf-8')).hexdigest())
+            # just use our cleaned article link
+            print("Hashed ID is: " + hashlib.sha256(cleanArticleLink.encode('utf-8')).hexdigest())
         
         print("\n")
 
