7464376f390cf219ac342737c438a128b38fa634
unknown <contact@benjaminwulf.com>
2026-04-28 11:48:32 -0400

    added better pathing to prevent director issues

--- db/init_db.py
+++ db/init_db.py
@@ -1,12 +1,17 @@
+from pathlib import Path
 import sqlite3
 
+DB_FOLDER = Path(__file__).parent.absolute()
+SCHEMA_PATH = DB_FOLDER / "schema.sql"
+DATABASE_PATH = DB_FOLDER / "rss_reader.db"
+
 def initDB():
-    connection = sqlite3.connect('rss_reader.db')
+    connection = sqlite3.connect(DATABASE_PATH)
     connection.execute("PRAGMA foreign_keys = ON")
-    with open('./schema.sql') as f:
+    with open(SCHEMA_PATH) as f:
         connection.executescript(f.read())
     connection.close()
+    print(f"Database initialized at: {DATABASE_PATH}")
 
 if __name__ == "__main__":
     initDB()
-    print("Database Initialized")
