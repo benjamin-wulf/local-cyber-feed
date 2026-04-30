95a919c534f5346821b2d31fb297d57ecef81345
unknown <contact@benjaminwulf.com>
2026-04-30 10:07:10 -0400

    changed to display local time

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import sys
 from pathlib import Path
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 
 sys.path.append(str(Path(__file__).parent.parent))
 from db.connection import GetDBConnection
@@ -14,12 +16,11 @@ def index():
     
     feeds = conn.execute('SELECT * FROM feeds').fetchall()
 
-    articles = conn.execute('SELECT a.title, a.link, a.published_date, f.title as feed_name FROM articles a JOIN feeds f ON a.feed_id = f.id ORDER BY a.id DESC LIMIT 10').fetchall()
+    articles = conn.execute('SELECT a.title, a.link, a.published_date, f.title as feed_name FROM articles a JOIN feeds f ON a.feed_id = f.id ORDER BY a.published_date DESC LIMIT 10').fetchall()
 
     conn.close()
 
     #should have gotten feeds and 10 recent articles, now need to give the data to the HTML template
-
     return render_template('index.html', feeds=feeds, articles=articles)
 
 if __name__ == '__main__':
