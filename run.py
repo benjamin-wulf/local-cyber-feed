d6e53f8aa2905a2ff11822ea64a5098cf5f4c48c
unknown <contact@benjaminwulf.com>
2026-05-08 13:34:43 -0400

    primary orchestration moved to run.py

--- run.py
+++ run.py
@@ -4,12 +4,6 @@ from db.connection import GetDBConnection
 from pathlib import Path
 import os
 
-
-#DB_FOLDER = Path(__file__).parent.absolute()
-#SCHEMA_PATH = DB_FOLDER / "schema.sql"
-#DATABASE_PATH = DB_FOLDER / "rss_reader.db"
-
-
 if __name__ == "__main__":
     # Check if DB exists, if not then create it.
     print(f"Testing path: {DATABASE_PATH}")
@@ -21,8 +15,8 @@ if __name__ == "__main__":
         #db file found, do nothing
         print("INFO: Found database file")
     # Launch the flask application
-    #if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-    #    scheduler.start()
-    #    print("INFO: Scheduler started in main worker process")
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        scheduler.start()
+        print("INFO: Scheduler started in main worker process")
 
-    #app.run(debug=True)
+    app.run(debug=True)
